09d9fca5f4405dc99ec5dabe8a11d1e56140026e
Mana <mehraein@usc.edu>
2018-04-25 22:35:39 -0700

    new changes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
  * Developed by:
  * Autonomous Networks Research Group (ANRG)
@@ -186,22 +186,23 @@ int main()
        at a speed of 25 (speed can be between -127 to 127) for 100 ms. Use
        functions like this in your program to move your m3pi when you get 
        MQTT messages! */
-    // movement('w', 25, 100);
-    // movement('w', 25, 100);
-    // movement('w', 25, 100);
-    // movement('w', 25, 100);
-    // movement('a', 25, 100);
-    // movement('a', 25, 100);
-    // movement('a', 25, 100);
-    // movement('a', 25, 100);
-    // movement('d', 25, 100);
-    // movement('d', 25, 100);
-    // movement('d', 25, 100);
-    // movement('d', 25, 100);
-    // movement('s', 25, 100);
-    // movement('s', 25, 100);
-    // movement('s', 25, 100);
-    // movement('s', 25, 100);
+    movement('w', 25, 100);
+    movement('w', 25, 100);
+    movement('w', 25, 100);
+    movement('w', 25, 100);
+    movement('a', 25, 100);
+    movement('a', 25, 100);
+    movement('a', 25, 100);
+    movement('a', 25, 100);
+    movement('d', 25, 100);
+    movement('d', 25, 100);
+    movement('d', 25, 100);
+    movement('d', 25, 100);
+    movement('s', 25, 100);
+    movement('s', 25, 100);
+    movement('s', 25, 100);
+    movement('s', 25, 100);
+    // print("Hello", 5)
 
     wait(1); //delay startup 
     printf("Resetting ESP8266 Hardware...\n");
